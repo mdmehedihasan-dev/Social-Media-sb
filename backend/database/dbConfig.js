e27915fdd929ea916d3130cbae581e324d6de7d5
Md Mehedi Hasan <116740305+mdmehedihasan-dev@users.noreply.github.com>
2025-05-15 21:00:20 +0600

    Update dbConfig.js
    Signed-off-by: Md Mehedi Hasan  <116740305+mdmehedihasan-dev@users.noreply.github.com>

--- backend/database/dbConfig.js
+++ backend/database/dbConfig.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose")
 
 exports.dbConnection =()=>{
     mongoose.connect(process.env.MONGODB_URL).then(
-        console.log("Database Connection Successfully✌️")
+        console.log("Database Connection Successfully")
     )
 }
