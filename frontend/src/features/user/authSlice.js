2cfc3455e5d033977cef629c686c778ba34e4e6b
Md Mehedi Hasan <116740305+mdmehedihasan-dev@users.noreply.github.com>
2025-05-15 20:43:43 +0600

    Update authSlice.js
    Signed-off-by: Md Mehedi Hasan  <116740305+mdmehedihasan-dev@users.noreply.github.com>

--- frontend/src/features/user/authSlice.js
+++ frontend/src/features/user/authSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     userInfo:JSON.parse(localStorage.getItem("user")) || null
